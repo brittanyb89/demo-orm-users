ed501ffc78fc29bdd78f96e0ec18c95c6ef4fe45
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2023-01-12 19:53:19 -0600

    Add new method to find user by username

--- app/user/controller.js
+++ app/user/controller.js
@@ -4,4 +4,9 @@ export default {
   create(payload) {
     return User.create(payload);
   },
+
+  // Add a new method to find user by username
+  show(username) {
+    return User.findOne({ where: { username } });
+  },
 };
